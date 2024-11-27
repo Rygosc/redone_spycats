5361b1c96e48b3762127418285368dbd63673426
Rygos <danayazetta@gmail.com>
2024-11-28 00:48:53 +0200

    updated urls

--- spyapp/urls.py
+++ spyapp/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import SpyCatDetailView
+from .views import SpyCatDetailView, MissionView, TargetsView
 
 urlpatterns = [
     path("spycats/", SpyCatDetailView.as_view(), name="spycat-list-create"),
     path("spycats/<int:pk>/", SpyCatDetailView.as_view(), name="spycat-detail"),
+    path("missions/", MissionView.as_view(), name="missions"),
+    path("missions/<int:pk>/", MissionView.as_view()),
+    path("targets/", TargetsView.as_view(), name="targets"),
+    path("targets/<int:pk>/", TargetsView.as_view()),
 ]
