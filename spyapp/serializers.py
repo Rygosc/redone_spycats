e01c288719a1998e86cefb6437299d24cd3009d3
Rygos <danayazetta@gmail.com>
2024-11-28 00:47:14 +0200

    created new serializers: TargetSerializer MissionSerializer

--- spyapp/serializers.py
+++ spyapp/serializers.py
@@ -1,8 +1,33 @@
 from rest_framework import serializers
-from .models import SpyCat
+from .models import SpyCat, Mission, Target 
 
 
 class SpyCatSerializer(serializers.ModelSerializer):
     class Meta:
         model = SpyCat
         fields = ["id", "name", "years_of_experience", "breed", "salary"]
+
+
+class TargetSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Target
+        fields = ["id", "mission", "name", "country", "notes", "complete"]
+
+
+class MissionSerializer(serializers.ModelSerializer):
+    targets = TargetSerializer(many=True)
+
+    class Meta:
+        model = Mission
+        fields = ["id", "cat", "complete", "created_at", "updated_at", "targets"]
+
+    def create(self, validated_data):
+        targets_data = validated_data.pop("targets", [])
+        mission = Mission.objects.create(
+            **validated_data
+        )
+        for target_data in targets_data:
+            Target.objects.create(mission=mission, **target_data)
+
+        mission.refresh_from_db()
+        return mission
